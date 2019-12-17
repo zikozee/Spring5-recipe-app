32e8518312bf414ac5fe954718d24a6d80afaf36
Ezekiel Eromosei <ezekiel.eromosei@gmail.com>
2019-12-17 09:45:42 +0100

    circle ci

---
+++ src/main/java/com/zikozee/services/IngredientService.java
@@ -0,0 +1,15 @@
+package com.zikozee.services;
+
+import com.zikozee.commands.IngredientCommand;
+
+/**
+ * Created by jt on 6/27/17.
+ */
+public interface IngredientService {
+
+    IngredientCommand findByRecipeIdAndIngredientId(Long recipeId, Long ingredientId);
+
+    IngredientCommand saveIngredientCommand(IngredientCommand command);
+
+    void deleteById(Long recipeId, Long idToDelete);
+}
---
+++ src/main/java/com/zikozee/services/RecipeService.java
@@ -0,0 +1,22 @@
+package com.zikozee.services;
+
+import com.zikozee.commands.RecipeCommand;
+import com.zikozee.domain.Recipe;
+
+import java.util.Set;
+
+/**
+ * Created by jt on 6/13/17.
+ */
+public interface RecipeService {
+
+    Set<Recipe> getRecipes();
+
+    Recipe findById(Long l);
+
+    RecipeCommand findCommandById(Long l);
+
+    RecipeCommand saveRecipeCommand(RecipeCommand command);
+
+    void deleteById(Long idToDelete);
+}
